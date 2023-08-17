a57e2eafa6264064d5bdd1f612cf7d1e3adea92d
samandar <xkas2001@gmail.com>
2023-08-17 13:04:07 +0500

    Added Mux library

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" {
+	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
 		fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -18,6 +18,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", mux)
 }
