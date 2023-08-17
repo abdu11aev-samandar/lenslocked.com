2a7cca99c51f02f381b7980c175b3d88c1cee18a
samandar <xkas2001@gmail.com>
2023-08-17 13:37:27 +0500

    Implemented the Gorilla/Mux router

--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-	} else if r.URL.Path == "/contact" || r.URL.Path == "/contact/" {
-		fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprint(w, "hello,%s!\n", ps.ByName("name"))
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send and email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/hello/:name", Hello)
-
-	//http.HandleFunc("/", handlerFunc)
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
 	http.ListenAndServe(":3000", router)
 }
