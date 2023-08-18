0972feb7560301aba75e1612341b0f9a3f1ec81e
samandar <xkas2001@gmail.com>
2023-08-18 16:09:09 +0500

    Added a Render method to our View type to simplify view rendering

--- views/view.go
+++ views/view.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
 )
 
@@ -28,6 +29,13 @@ type View struct {
 	Layout   string
 }
 
+// Render is used to render the view with the predefined layout
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+// layoutFiles returns a slice of strings representing
+// the layout files used in our application.
 func layoutFiles() []string {
 	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
 	if err != nil {
